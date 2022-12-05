bd22cb650ab9405cbfa33020c545feb60b3e13d6
hammadshifa97 <62197272+hammadshifa97@users.noreply.github.com>
2022-12-05 19:48:41 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -45,18 +45,17 @@ function App() {
       <Alert alert={alert}/>
       <Textbox showAlert={showAlert} mode={mode}/>*/
       <h1> Funny Videos 2022 | Instant Regret | Fails Of The Week | Fail Compilation 2022 | Fails | RandomFails </h1>
-      <br/>
+      \n
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8649682487259297"
      crossorigin="anonymous"></script>
 <ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-8649682487259297" data-ad-slot="5009537847"></ins>
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
-    <br/>
-    <iframe width="100%" height="400px" src="https://www.youtube.com/embed/yXG7wXI5U2I" title="Funny Videos 2022 | Instant Regret | Fails Of The Week | Fail Compilation 2022 | Fails | RandomFails" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+\n    <iframe width="100%" height="400px" src="https://www.youtube.com/embed/yXG7wXI5U2I" title="Funny Videos 2022 | Instant Regret | Fails Of The Week | Fail Compilation 2022 | Fails | RandomFails" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     
-     <br/>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8649682487259297"
+\n
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8649682487259297"
      crossorigin="anonymous"></script>
 <ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-8649682487259297" data-ad-slot="5009537847"></ins>
 <script>
