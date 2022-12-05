128b8af32d8d9dbf771c092fb150b30640eca58a
hammadshifa97 <62197272+hammadshifa97@users.noreply.github.com>
2022-12-05 19:52:15 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -36,6 +36,15 @@ function App() {
         );
       }
     }
+    
+    const ads = ()=>{
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8649682487259297"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-8649682487259297" data-ad-slot="5009537847"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+    }
 
 
   return (
@@ -46,21 +55,12 @@ function App() {
       <Textbox showAlert={showAlert} mode={mode}/>*/
       <h1> Funny Videos 2022 | Instant Regret | Fails Of The Week | Fail Compilation 2022 | Fails | RandomFails </h1>
       \n
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8649682487259297"
-     crossorigin="anonymous"></script>
-<ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-8649682487259297" data-ad-slot="5009537847"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+      {ads}
+      
 \n    <iframe width="100%" height="400px" src="https://www.youtube.com/embed/yXG7wXI5U2I" title="Funny Videos 2022 | Instant Regret | Fails Of The Week | Fail Compilation 2022 | Fails | RandomFails" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    
+{ads}
 \n
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8649682487259297"
-     crossorigin="anonymous"></script>
-<ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-8649682487259297" data-ad-slot="5009537847"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+    
     </div>
   );
 }
